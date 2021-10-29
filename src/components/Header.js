a71fea29fa98bc688ea9fa6ee48ca48289ecc025
Gabriel Bolea <gabriel.bolea@outlook.com>
2021-10-29 17:42:37 +0100

    Create Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
+import Button from "./Button"
 const Header = () => {
     return (
         <header className="header">
             <h1>Registo de Viaturas</h1>
-            <button className="btn">Adicionar</button>
+            <Button color="blue" text="Adicionar" />
         </header>
     )
 }
